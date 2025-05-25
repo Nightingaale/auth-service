15ebe64eb1226187fc46a4ae5a9f9a787b144440
Nightingaale <shirounightingaleoff@gmail.com>
2025-05-26 00:42:54 +0300

    feature: update controller(post requests)

--- src/main/java/org/nightingaale/authservice/controller/AuthServiceController.java
+++ src/main/java/org/nightingaale/authservice/controller/AuthServiceController.java
@@ -1,12 +1,54 @@
 package org.nightingaale.authservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.nightingaale.authservice.dto.*;
+import org.nightingaale.authservice.listener.AuthServiceListener;
+import org.nightingaale.authservice.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthServiceController {
 
+    private final AuthServiceListener authDtoListener;
+    private final BCryptPasswordEncoder passwordEncoder;;
+    private final AuthService authService;
+
+
+    @PostMapping("/sign-up")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> signUp(@RequestBody UserRegistrationDto event) {
+        event.setCorrelationId(UUID.randomUUID().toString());
+        event.setPassword(passwordEncoder.encode(event.getPassword()));
+        authDtoListener.saveRegistrationEvent(event);
+        return ResponseEntity.ok("User has successfully been signed up!");
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> signIn(@RequestBody UserLoginDto event) {
+        event.setCorrelationId(UUID.randomUUID().toString());
+        authDtoListener.saveLoginEvent(event);
+        return ResponseEntity.ok("User has successfully been signed in!");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@AuthenticationPrincipal Jwt jwt) {
+        authService.logoutUser(jwt.getSubject());
+        return ResponseEntity.ok("User has successfully been logged out!");
+    }
+
+    @PostMapping("/remove-user")
+    public ResponseEntity<?> removeUser(@RequestBody UserRemoveDto event) {
+        event.setCorrelationId(UUID.randomUUID().toString());
+        authDtoListener.saveRemoveEvent(event);
+        return ResponseEntity.ok("User with ID: " + event.getUserId() + "has successfully been removed!");
+    }
 }
