47af303d5091efc3e8e9417a4e881a830339bd83
Nightingaale <shirounightingaleoff@gmail.com>
2025-05-28 20:24:22 +0300

    feature: update ServiceApplication

--- src/main/java/org/nightingaale/authservice/AuthServiceApplication.java
+++ src/main/java/org/nightingaale/authservice/AuthServiceApplication.java
@@ -2,10 +2,12 @@ package org.nightingaale.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableCaching
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
