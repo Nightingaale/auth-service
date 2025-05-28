0f19de356a501a1ab580ed71a020d21249ab64c9
Nightingaale <shirounightingaleoff@gmail.com>
2025-05-29 01:23:39 +0300

    feature: update login & logout repository

--- src/main/java/org/nightingaale/authservice/repository/UserLogoutRepository.java
+++ src/main/java/org/nightingaale/authservice/repository/UserLogoutRepository.java
@@ -4,9 +4,6 @@ import org.nightingaale.authservice.entity.UserLogoutEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserLogoutRepository extends JpaRepository<UserLogoutEntity, Long> {
-    Optional<UserLogoutEntity> findByCorrelationId(String correlationId);
+public interface UserLogoutRepository extends JpaRepository<UserLogoutEntity, String> {
 }
