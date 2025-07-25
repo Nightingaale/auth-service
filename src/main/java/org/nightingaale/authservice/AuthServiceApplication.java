bff7954cdf7010b8946a126073e179cfdcf3ea2c
Nightingaale <shirounightingaleoff@gmail.com>
2025-07-26 01:59:28 +0300

    feature: discovery client for eureka

--- src/main/java/org/nightingaale/authservice/AuthServiceApplication.java
+++ src/main/java/org/nightingaale/authservice/AuthServiceApplication.java
@@ -3,11 +3,13 @@ package org.nightingaale.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableCaching
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
