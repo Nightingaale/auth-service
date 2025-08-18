f2c0f235b20e93f5dd835b4a72f4cbbfb0da756a
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-19 00:57:11 +0300

    feature: kafka listeners

---
+++ src/main/java/org/nightingaale/authservice/event/UserRemovedEvent.java
@@ -0,0 +1,30 @@
+package org.nightingaale.authservice.event;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.nightingaale.authservice.dto.UserRegisteredDto;
+import org.nightingaale.authservice.dto.UserRemovedDto;
+import org.nightingaale.authservice.listener.AuthServiceListener;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserRemovedEvent {
+
+    private final AuthServiceListener authServiceListener;
+
+    @KafkaListener(topics = "user-removed", groupId = "auth-service", containerFactory = "kafkaListenerContainerFactoryUserRemoved")
+    public void userRemoved(UserRemovedDto userRemovedDto) {
+        log.info("Received user-remove Kafka event: {}", userRemovedDto);
+        authServiceListener.saveRemovedEvent(userRemovedDto);
+    }
+
+    @KafkaListener(topics = "user-registered", groupId = "auth-service", containerFactory = "kafkaListenerContainerFactoryUserRegistered")
+    public void saveRegisteredEvent(UserRegisteredDto event) {
+        if (event.isUserExists()) {
+            log.info("[User has been successfully registered with ID: {}]", event.getCorrelationId());
+        }
+    }
+}
