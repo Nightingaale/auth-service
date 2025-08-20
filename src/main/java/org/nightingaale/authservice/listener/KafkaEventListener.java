c7720d9a1a74db805b77e20a0c513eea95473ca6
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-21 01:15:30 +0300

    feature: add registered entity

--- src/main/java/org/nightingaale/authservice/listener/KafkaEventListener.java
+++ src/main/java/org/nightingaale/authservice/listener/KafkaEventListener.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.nightingaale.authservice.dto.UserRegisteredDto;
 import org.nightingaale.authservice.dto.UserRemovedDto;
+import org.nightingaale.authservice.entity.UserRegisteredEntity;
+import org.nightingaale.authservice.entity.UserRegistrationEntity;
+import org.nightingaale.authservice.mapper.UserRegisteredMapper;
+import org.nightingaale.authservice.repository.UserRegisteredRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +17,20 @@ import org.springframework.stereotype.Service;
 public class KafkaEventListener {
 
     private final AuthServiceListener authServiceListener;
+    private final UserRegisteredRepository userRegisteredRepository;
+    private final UserRegisteredMapper userRegisteredMapper;
 
     @KafkaListener(topics = "user-removed", groupId = "auth-service", containerFactory = "kafkaListenerContainerFactoryUserRemoved")
     public void userRemoved(UserRemovedDto userRemovedDto) {
-        log.info("[Received user-removed Kafka event from user-service: {}]", userRemovedDto);
         authServiceListener.saveRemovedEvent(userRemovedDto);
+        log.info("[Received user-removed Kafka event from user-service: {}]", userRemovedDto);
     }
 
     @KafkaListener(topics = "user-registered", groupId = "auth-service", containerFactory = "kafkaListenerContainerFactoryUserRegistered")
     public void saveRegisteredEvent(UserRegisteredDto event) {
         if (event.isUserExists()) {
+            UserRegisteredEntity entity = userRegisteredMapper.toEntity(event);
+            userRegisteredRepository.save(entity);
             log.info("[Received user-registered Kafka event from user-service. User has been successfully registered with ID: {}]", event.getCorrelationId());
         }
     }
