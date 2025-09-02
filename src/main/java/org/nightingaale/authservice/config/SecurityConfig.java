36e5c98ea52c09908791cfc64d3d2bc15ed9f12f
Nightingaale <shirounightingaleoff@gmail.com>
2025-09-02 16:04:06 +0300

    refactor: remove swagger

--- src/main/java/org/nightingaale/authservice/config/SecurityConfig.java
+++ src/main/java/org/nightingaale/authservice/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/sign-up", "/api/v1/auth/sign-in").permitAll()
-                        .requestMatchers("/v1/openapi-docs/**", "/swagger-ui/**", "/actuator/**", "/aggregate/**").permitAll()
+                        .requestMatchers("/actuator/**", "/aggregate/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2
