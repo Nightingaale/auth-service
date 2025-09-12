a217f4d78ff14f70103e63d3773261f2ed408204
Artem <shirounightingaleoff@gmail.com>
2025-09-13 02:17:35 +0300

    refactor: remove caching

--- src/main/java/org/nightingaale/authservice/AuthServiceApplication.java
+++ src/main/java/org/nightingaale/authservice/AuthServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-@EnableCaching
 @EnableDiscoveryClient
 public class AuthServiceApplication {
 
