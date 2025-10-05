5f745cb25de1e169d6cb9b547b612562f346a98e
Nightingaale <shirounightingaleoff@gmail.com>
2025-10-05 17:33:58 +0300

    refactor: remove mapping target from login entity

--- src/main/java/org/nightingaale/authservice/mapper/UserUpdateRequestMapper.java
+++ src/main/java/org/nightingaale/authservice/mapper/UserUpdateRequestMapper.java
@@ -3,7 +3,6 @@ package org.nightingaale.authservice.mapper;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.mapstruct.*;
 import org.nightingaale.authservice.event.KafkaUserUpdateRequestEvent;
-import org.nightingaale.authservice.model.entity.UserLoginEntity;
 import org.nightingaale.authservice.model.entity.UserRegistrationEntity;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
@@ -14,5 +13,4 @@ public interface UserUpdateRequestMapper {
     void toEvent(KafkaUserUpdateRequestEvent event, @MappingTarget UserRepresentation userRep);
 
     void updateFromKeycloak(UserRepresentation userRep, @MappingTarget UserRegistrationEntity entity);
-    void updateToLoginEntity(UserRepresentation userRep, @MappingTarget UserLoginEntity loginEntity);
 }
